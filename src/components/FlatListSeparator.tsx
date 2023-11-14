fe67bf66a8de88854541789979d54110b75b1a81
AnyMal <lksdro01@gmail.com>
2023-11-14 19:50:28 -0300

    Agregar componente separador

---
+++ src/components/FlatListSeparator.tsx
@@ -0,0 +1,15 @@
+import { View } from "react-native"
+
+const FlatListSeparator = () => {
+    return(
+        <View 
+            style={{
+                height: 1,
+                width: '100%',
+                backgroundColor: '#999'
+            }}
+        />
+    )
+}
+
+export default FlatListSeparator;
